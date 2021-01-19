2b44d5e79b6069dd3a8206f170d34f9b04eef59b
ixnrobmaurizio <47404089+ixnrobmaurizio@users.noreply.github.com>
2021-01-19 09:26:01 -0600

    removed redunant commands

--- scripts/softwareUpdate.py
+++ scripts/softwareUpdate.py
@@ -7,10 +7,10 @@ print('\nChecking for software updates via Brew...')
 # Updates Homebrew, taps, and repos
 os.system('brew update ; brew upgrade ; brew upgrade --cask')
 
-print('\nUpdates are done. Now for some cleanup...')
+print('\nUpdates are done. Cleaning up...')
 
 # Cleanup after running updates
-os.system('brew cleanup ; brew cleanup -s')
+os.system('brew cleanup')
 os.system("find . -type f -name '*.DS_Store' -ls -delete")
 
 print('Cleanup is done. Checking for macOS updates...\n')
@@ -18,6 +18,4 @@ print('Cleanup is done. Checking for macOS updates...\n')
 # Checks for macOS updates and install all available
 os.system('softwareupdate -ia')
 
-print('\nFinished! Your system is ready.\n')
-
-os.system('exit')
+print('\nFinished!')
